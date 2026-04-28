4c17b5ceb34728638e48ed8c1ccd1bdc380fbda2
kris3713 <07e5297d5b@c0x0.com>
2026-04-28 06:11:07 -0600

    Updates
    Signed-off-by: kris3713 <07e5297d5b@c0x0.com>

--- config.el
+++ config.el
@@ -1,10 +1,8 @@
 ;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
 
-(add-to-list 'initial-frame-alist '(fullscreen . maximized))
-
 ;; Place your private configuration here! Remember, you do not need to run 'doom
 ;; sync' after modifying this file!
-
+(add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
 ;; Some functionality uses this to identify you, e.g. GPG configuration, email
 ;; clients, file templates and snippets. It is optional.
